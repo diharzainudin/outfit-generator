6b9640b994b7ab7f927db9728faae270dda34f7c
diharzainudin <diharzainudin@gmail.com>
2024-02-27 22:13:27 +0800

    5 added photos in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,64 @@
 const Footer = (props) => {
   return (
     <footer class="footerbar">
-      <a
-        href="https://www.facebook.com/Vogue"
-        class="fa fa-facebook"
-        target="_blank"
-      >
-        {' '}
-        Facebook{' '}
-      </a>
-      <a
-        href="https://www.instagram.com/vogue/?hl=en"
-        class="fa fa-instagram"
-        target="_blank"
-      >
-        {' '}
-        Instagram{' '}
-      </a>
-      <a
-        href="https://www.youtube.com/@Vogue"
-        class="fa fa-youtube"
-        target="_blank"
-      >
-        {' '}
-        YouTube{' '}
-      </a>
+      <div class="carousel">
+        <a href="#" class="gallery">
+          <img src="https://i.pinimg.com/564x/61/63/27/616327f7c9a71426b5a01f543fe9257a.jpg" />
+        </a>
+        <a href="#" class="gallery">
+          <img src="https://i.pinimg.com/564x/39/73/84/39738440efff0e25542f99321f7ff49b.jpg" />
+        </a>
+        <a href="#" class="gallery">
+          <img src="https://i.pinimg.com/564x/4e/00/60/4e00601722048c543c04d1e24c877be7.jpg" />
+        </a>
+        <a href="#" class="gallery">
+          <img src="https://i.pinimg.com/564x/ad/2d/c4/ad2dc4013469e8eb394913997c36414e.jpg" />
+        </a>
+        <a href="#" class="gallery">
+          <img src="https://i.pinimg.com/564x/30/6a/32/306a32717eea573f89bf9fe35563461f.jpg" />
+        </a>
+        <a href="#" class="gallery">
+          <img src="https://i.pinimg.com/564x/8c/21/0d/8c210dcaa49342561e900ecd335a1691.jpg" />
+        </a>
+        <a href="#" class="gallery">
+          <img src="https://i.pinimg.com/736x/49/f8/29/49f8299293b465c61167b30bd808c82d.jpg" />
+        </a>
+      </div>
+
+      <div class="footer-icons">
+        <ul class="footer-wrapper">
+          <li>
+            <a
+              href="https://www.facebook.com/Vogue"
+              class="fa fa-facebook"
+              target="_blank"
+            >
+              {' '}
+              Facebook{' '}
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/vogue/?hl=en"
+              class="fa fa-instagram"
+              target="_blank"
+            >
+              {' '}
+              Instagram{' '}
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/@Vogue"
+              class="fa fa-youtube"
+              target="_blank"
+            >
+              {' '}
+              YouTube{' '}
+            </a>{' '}
+          </li>
+        </ul>
+      </div>
     </footer>
   )
 }
