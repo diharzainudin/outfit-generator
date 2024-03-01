2d874af425cebf2793ca4f564f3c58f0732d15de
diharzainudin <diharzainudin@gmail.com>
2024-03-01 23:45:18 +0800

    15 added brand name over each image

--- src/components/Clothes.jsx
+++ src/components/Clothes.jsx
@@ -5,11 +5,14 @@ const Clothes = (props) => {
   return (
     <div class="clothes-container">
       <li classname="clothes" id={props.key}>
-        <h3>Brand: {props.brand}</h3>
+        {/* <h3>Brand: {props.brand}</h3> */}
         <p>
           <i>{props.title}</i>
         </p>
         <img src={props.image} class="clothes-img" />
+        <div class="overlay">
+          <h3 class="brand">{props.brand}</h3>
+        </div>
         <h3 id="price">{props.price} </h3>
         <h3 id="buy-button">
           <Button link={props.link} />
