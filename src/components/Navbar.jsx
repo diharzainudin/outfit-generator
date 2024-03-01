5c26866900eb183d92fc32613ccc45228c461df5
diharzainudin <diharzainudin@gmail.com>
2024-03-01 22:24:15 +0800

    10 aligned login form with navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,32 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = (props) => {
   return (
-    <nav>
-      <ul class="navbar">
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/mycloset">My Closet</NavLink>
-        </li>
-        <li>
-          <NavLink to="/wishlist">Wishlist</NavLink>
-        </li>
-        <li>
-          <NavLink to="/styleme">Style Me</NavLink>
-        </li>
-      </ul>
-      <div class="login-container">
-        <form action="/action_page.php">
-          <input type="text" placeholder="Username" name="username"></input>
-          <input type="text" placeholder="Password" name="psw"></input>
-          <button type="submit">Login</button>
-        </form>
-      </div>
+    <nav class="navbar">
+      <section>
+        <ul class="navlinks">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/mycloset">My Closet</NavLink>
+          </li>
+          <li>
+            <NavLink to="/wishlist">Wishlist</NavLink>
+          </li>
+          <li>
+            <NavLink to="/styleme">Style Me</NavLink>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <div class="login-container">
+          <form action="/action_page.php">
+            <input type="text" placeholder="Username" name="username"></input>
+            <input type="text" placeholder="Password" name="psw"></input>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </section>
     </nav>
   )
 }
